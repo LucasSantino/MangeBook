2786dec782b5cdb1cde08af43514b418dc64f271
LucasSantino <lsantino541@gmail.com>
2024-11-04 12:28:53 -0300

    .

--- Site - MangeBook/Js/NavSidebar.js
+++ Site - MangeBook/Js/NavSidebar.js
@@ -39,3 +39,53 @@ function toggleProfileDropdown() {
     dropdownMenu.style.display = "none"; // Oculta o dropdown
   }
 }
+
+/// Função da Side bar - Dropdawn Menu ADiministrador ///
+
+// Função para alternar a visibilidade do menu Dropdown "Gerenciamento (Livros)"
+function toggleBooksDropdown() {
+  const dropdownMenu = document.getElementById("dropdown-menu-books");
+  const setaIcon = document.getElementById("seta-icon-books");
+
+  dropdownMenu.classList.toggle("visible"); // Alterna a visibilidade do dropdown
+  setaIcon.classList.toggle("rotacionado"); // Alterna a rotação da seta
+
+  // Altera a propriedade de display com base na visibilidade
+  if (dropdownMenu.classList.contains("visible")) {
+    dropdownMenu.style.display = "block"; // Mostra o dropdown
+  } else {
+    dropdownMenu.style.display = "none"; // Oculta o dropdown
+  }
+}
+
+// Função para alternar a visibilidade do menu Dropdown "Gerenciamento (Usuários)"
+function toggleUsersDropdown() {
+  const dropdownMenu = document.getElementById("dropdown-menu-users");
+  const setaIcon = document.getElementById("seta-icon-users");
+
+  dropdownMenu.classList.toggle("visible"); // Alterna a visibilidade do dropdown
+  setaIcon.classList.toggle("rotacionado"); // Alterna a rotação da seta
+
+  // Altera a propriedade de display com base na visibilidade
+  if (dropdownMenu.classList.contains("visible")) {
+    dropdownMenu.style.display = "block"; // Mostra o dropdown
+  } else {
+    dropdownMenu.style.display = "none"; // Oculta o dropdown
+  }
+}
+
+// Função para alternar a visibilidade do menu Dropdown "Estatísticas"
+function toggleStatisticsDropdown() {
+  const dropdownMenu = document.getElementById("dropdown-menu-statistics");
+  const setaIcon = document.getElementById("seta-icon-statistics");
+
+  dropdownMenu.classList.toggle("visible"); // Alterna a visibilidade do dropdown
+  setaIcon.classList.toggle("rotacionado"); // Alterna a rotação da seta
+
+  // Altera a propriedade de display com base na visibilidade
+  if (dropdownMenu.classList.contains("visible")) {
+    dropdownMenu.style.display = "block"; // Mostra o dropdown
+  } else {
+    dropdownMenu.style.display = "none"; // Oculta o dropdown
+  }
+}
