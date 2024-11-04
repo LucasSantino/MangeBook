06f3d34454ee11e8783f55fb907ebefeaa46b98e
LucasSantino <lsantino541@gmail.com>
2024-11-03 21:15:07 -0300

    PerfilUsuario --- Finalizado

---
+++ Site - MangeBook/Js/PerfilUsario.js
@@ -0,0 +1,13 @@
+function editarPerfil() {
+    alert("Função de edição de perfil em desenvolvimento.");
+  }
+  
+  function alterarSenha() {
+    alert("Função de alteração de senha em desenvolvimento.");
+  }
+  
+  function sair() {
+    alert("Sessão encerrada. Você será redirecionado para a página inicial.");
+    window.location.href = "index.html";
+  }
+  
