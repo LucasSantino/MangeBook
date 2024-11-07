f18bb5d2515fb1719f2b8bb866af8c5d6a32c807
LucasSantino <lsantino541@gmail.com>
2024-11-06 22:37:45 -0300

    Alteração paginaçoes

--- libraryFrontend/user-frontend/src/router/index.js
+++ libraryFrontend/user-frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'; 
 import Login from '../views/Login.vue'; 
-import Dashboard from '../views/Dashboard.vue'; 
+
 
 const routes = [
   {
@@ -8,12 +8,7 @@ const routes = [
     name: 'Login',
     component: Login,
   },
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    meta: { requiresAuth: true },
-  },
+ 
   {
     path: '/:catchAll(.*)', // Usando a nova sintaxe para rotas coringa
     redirect: '/login',
