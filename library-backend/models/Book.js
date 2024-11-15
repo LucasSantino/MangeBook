c4553fd957e3ea1deeb689cf71692ac1bb021e46
LucasSantino <lsantino541@gmail.com>
2024-11-14 22:48:58 -0300

    alteração nas rotas

--- library-backend/models/Book.js
+++ library-backend/models/Book.js
@@ -5,7 +5,20 @@ const BookSchema = new mongoose.Schema({
     title: {type: String, required: true},
     author: {type: String, required: true},
     year: {type: Number},
+    genre:{type:String, required:true},
+    ISBN:{type: String, required: true},
+    copies:{type:Number, required:true},
+    description:{type:String, required: true},
+    coverImage: { type: String },
+    reviews: [
+        {
+            // userId: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+            rating: { type: Number }, // Referência para o livro
+            comment: { type: String },
+            reviewsDate: { type: Date },
+        },
+    ],
 });
 
-// Exportando o modelo 
+// Exportando o modelo
 module.exports = mongoose.model('Book', BookSchema);
