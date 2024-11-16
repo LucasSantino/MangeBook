d01a90a7d4584ae4d3193f7227aab3c832dbfed0
LucasSantino <lsantino541@gmail.com>
2024-11-16 13:20:40 -0300

    Criação da pagina MeusLivros.vue

---
+++ libraryFrontend/user-frontend/src/views/MeusLivros.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+    <NavBar @toggle-sidebar="toggleSidebar" /> <!-- Passando o evento para abrir/fechar a Sidebar -->
+    <SideBar :isSidebarOpen="sidebarOpen" @toggle-sidebar="toggleSidebar" /> <!-- Passando o estado da Sidebar -->
+    <MainContent /> <!-- Importação do Conteúdo Principal-->
+    </div>
+
+
+</template>
+
+<script>
+
+import NavBar from '@/components/NavBar.vue';
+import SideBar from '@/components/SideBar.vue';
+import Footer from '@/components/Footer.vue';
+
+export default {
+  components: {
+    NavBar,
+    SideBar,
+  },
+
+
+  
+}
+
+
+</script>
+
+<style scoped>
+
+</style>
